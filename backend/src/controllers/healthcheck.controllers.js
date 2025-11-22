20cbc071b71990bb9c5356d0c7d85aa713b16021
rameshwar mane <manerameshwar909@gmail.com>
2025-11-22 15:27:36 +0530

    feat: refactor health check controller to use async/await and add error handling; implement asyncHandler utility

--- backend/src/controllers/healthcheck.controllers.js
+++ backend/src/controllers/healthcheck.controllers.js
@@ -1,8 +1,13 @@
 import {ApiResponse} from "../utils/api-response.js";
 
-const healthCheck= (req,res)=>{
-    res.status(200).json(
+const healthCheck= async(req,res)=>{
+   try {
+    
+     res.status(200).json(
         new ApiResponse(200,{message: "Server is running"})
     )
+   } catch (error) {
+    
+   }
 }
 export {healthCheck};
--- backend/src/utils/async-handler.js
+++ backend/src/utils/async-handler.js
@@ -1,3 +1,14 @@
+
+function asyncHandler(requestHandler){
+    return function(req,res,next){
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((error)=>next(error));
+    }
+}
+
+export {asyncHandler};
+
+
 // const asyncHandler = (requestHandler)=>{
 //     return (req,res,next)=>{
 //         Promise.resolve(requestHandler(req,res,next))
