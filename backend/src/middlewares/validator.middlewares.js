e9b77b22d70669631184d7ce0335af1ffce36785
rameshwar mane <manerameshwar909@gmail.com>
2025-11-23 18:36:00 +0530

    feat: implement user registration route with validation middleware

---
+++ backend/src/middlewares/validator.middlewares.js
@@ -0,0 +1,15 @@
+import { validationResult } from "express-validator";
+import {ApiError} from "../utils/api-error.js"
+
+export const validate = (req,res,next)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        const extractedErrors = [];
+        errors.array().map(err=>extractedErrors.push({
+            [err.path]: err.msg,
+        }));
+
+        throw new ApiError(422,"Validation Error", extractedErrors);
+    }
+    next();
+}
