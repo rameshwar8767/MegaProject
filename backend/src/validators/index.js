c76d19ebf1e81d647beeeed522bedbcac507c3cf
rameshwar mane <manerameshwar909@gmail.com>
2025-11-23 18:14:08 +0530

    feat: add user registration and login validators using express-validator

---
+++ backend/src/validators/index.js
@@ -0,0 +1,46 @@
+import {body} from "express-validator";
+
+
+const userRegistrationValidator = ()=>{
+    return [
+        body("email")
+            .trim()
+            .notEmpty().withMessage("Email is required")
+            .isEmail().withMessage("Email is invalid"),
+
+        body("username")
+            .trim()
+            .notEmpty().withMessage("Username is required")
+            .isLength({ min: 3, max: 16 })
+            .withMessage("Username must be between 3 and 16 characters"),
+
+        body("password")
+            .trim()
+            .notEmpty().withMessage("Password is required")
+            .isLength({ min: 8 })
+            .withMessage("Password should be at least 8 characters long")
+            .isStrongPassword()
+            .withMessage("Password should contain uppercase, lowercase, number, and symbol"),
+
+        body("fullName")
+            .trim()
+            .notEmpty().withMessage("Full name is required"),
+    ];
+}
+
+const userLoginValidator = ()=>{
+    return[
+        body("email")
+            .trim()
+            .notEmpty().withMessage("Email is required")
+            .isEmail().withMessage("Email is invalid"),
+
+        body("password")
+            .trim()
+            .notEmpty().withMessage("Password is required")
+            .isLength({ min: 8 })
+            .withMessage("Password must be at least 8 characters"),
+    ];
+}
+
+export {userRegistrationValidator, userLoginValidator};
