f9f8172a8147872eed5f213a4f341ecf75bcf58c
rameshwar mane <manerameshwar909@gmail.com>
2025-11-26 18:04:46 +0530

    feat: implement getAllProjects function; fetch and return all projects with creator details

--- backend/src/controllers/project.controllers.js
+++ backend/src/controllers/project.controllers.js
@@ -1,4 +1,38 @@
-const getProjects = asyncHandler(async(req,res)=>{});
+import { Project } from "../models/project.models.js";
+import { ApiError } from "../utils/api-error.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handler";
+
+const getAllProjects = asyncHandler(async(req,res)=>{
+   
+    const projects = await Project.find()
+        .populate("createdBy","name email")
+        .sort({createdAt: -1});
+
+    if(!projects ||projects.length === 0){
+        return res
+        .status(200)
+        .json(
+        new ApiResponse(
+            200,
+           {projects: []},
+            "No projects found"
+        )
+        );
+    }
+
+    return res
+        .status(200)
+        .json(
+        new ApiResponse(
+            200,
+           projects,
+            "Projects fetched successfully"
+        )
+        );
+
+
+});
 
 const getProjectById = asyncHandler(async(req,res)=>{});
 
@@ -18,4 +52,4 @@ const updateProjectMemberRole = asyncHandler(async(req,res)=>{});
 
 const deleteProjectMember = asyncHandler(async(req,res)=>{});
 
-export {getProjects, getProjectById, createProject, updateProject, deleteProject, addMemberToProject, removeMemberFromProject, getProjectMembers, updateProjectMemberRole, deleteProjectMember};
+export {getAllProjects, getProjectById, createProject, updateProject, deleteProject, addMemberToProject, removeMemberFromProject, getProjectMembers, updateProjectMemberRole, deleteProjectMember};
