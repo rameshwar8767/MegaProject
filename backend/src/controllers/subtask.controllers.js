901a842e94f5f239353f126e21122537bbf5dcf3
rameshwar mane <manerameshwar909@gmail.com>
2025-12-01 22:43:23 +0530

    feat(subtask): implement create, fetch, update, and delete SubTask controllers

---
+++ backend/src/controllers/subtask.controllers.js
@@ -0,0 +1,21 @@
+
+
+const createSubTask = asyncHandler(async(req,res)=>{
+
+})
+
+const getSubTaskById = asyncHandler(async(req,res)=>{
+    
+})
+
+const getSubTasksByTask = asyncHandler(async(req,res)=>{
+    
+})
+
+const updateSubTask = asyncHandler(async(req,res)=>{
+    
+})
+
+const deleteSubTask = asyncHandler(async(req,res)=>{
+    
+})
