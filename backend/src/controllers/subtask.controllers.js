9ffb97341c412babfaf16f02b927d931c92965ca
rameshwar mane <manerameshwar909@gmail.com>
2026-02-22 15:53:46 +0530

    feat(subtask): add validation for task ID in getSubTasksByTask and updateSubTask functions

--- backend/src/controllers/subtask.controllers.js
+++ backend/src/controllers/subtask.controllers.js
@@ -53,11 +53,50 @@ const getSubTaskById = asyncHandler(async(req,res)=>{
 })
 
 const getSubTasksByTask = asyncHandler(async(req,res)=>{
-    
+    const {taskId} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(taskId)){
+        res.status(400);
+        throw new Error("Invalid Task ID");
+    }
+
+    const subTasks = await SubTask.find({
+        task: taskId,
+        createdBy: req.user._id,
+    }).sort({createdAt:-1});
+
+    res.status(200).json({
+        success:true,
+        count : subTasks.length,
+        data : subTasks
+    })
 })
 
 const updateSubTask = asyncHandler(async(req,res)=>{
-    
+    const {id} = req.params;
+    const { title, isCompleted, dueDate} = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(400);
+        throw new Error("Invalid SubTask ID");
+    }
+
+    const subTask = await SubTask.findOne({
+        _id: id,
+        createdBy: req.user._id,
+    });
+
+    if (title !== undefined) subTask.title = title;
+    if (isCompleted !== undefined) subTask.isCompleted = isCompleted;
+    if (dueDate !== undefined) subTask.dueDate = dueDate;
+
+    const updatedSubTask = await subTask.save();
+
+    res.status(200).json({
+        success: true,
+        message: "SubTask updated successfully",
+        data: updatedSubTask,
+    });
 })
 
 const deleteSubTask = asyncHandler(async(req,res)=>{
